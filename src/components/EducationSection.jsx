3aca4ed2fae2a0a1b02c27782eb117a02838b5b0
dmclink <135415819+dmclink@users.noreply.github.com>
2026-02-15 19:02:22 +0800

    feat: style education preview section

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -6,8 +6,10 @@ function EducationSection({ data }) {
 			<article className="education-entry">
 				<header className="education-header">
 					<h4 className="education-school-name">{entry.schoolName}</h4>
-					<p className="education-degree">{entry.schoolDegreeName}</p>
-					<p className="education-honors">{entry.schoolGPA}</p>
+					<p className="education-degree">
+						{entry.schoolDegreeName}
+						<span className="education-honors">{entry.schoolGPA && ', ' + entry.schoolGPA}</span>
+					</p>
 					<p className="education-date">{formatDates(entry.schoolFrom, entry.schoolTo)}</p>
 				</header>
 				<div className="education-description">{buildDescription(entry.schoolDescription)}</div>
